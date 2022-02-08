8ba5a02c00c5dc885283ca3da2173feea631ad8b
Amy <amykaletabe@gmail.com>
2022-02-08 15:20:32 +0100

    JavaScript and small changes

--- script/formler.js
+++ script/formler.js
@@ -1,14 +1,36 @@
 //variables
-let fodselsdato = $("fodselsdato");
-let kjønn = $("kjønn");
-let wowrace = $("wowrace");
-let antallGangerRettetGeirSinProg = $("antallGangerRettetGeirSinProg");
-let treningPerUke = $("treningPerUke");
-let McTjukkasPerManed = $("McTjukkasPerManed");
-let hasFrokostForSkole = $("hasFrokostForSkole");
-let hasDrivingFemalePartner = $("hasDrivingFemalePartner");
-let hasMaleDrivingPartner = $("hasMaleDrivingPartner");
-let isSpillerItimen = $("isSpillerItimen");
+const fodselsdato = document.getElementById("fodselsdato");
+const kjønn = document.getElementById("kjønn");
+const wowrace = document.getElementById("wowrace");
+const antallGangerRettetGeirSinProg = document.getElementById(
+  "antallGangerRettetGeirSinProg"
+);
+const treningPerUke = document.getElementById("treningPerUke");
+const McTjukkasPerManed = document.getElementById("McTjukkasPerManed");
+const hasFrokostForSkole = document.getElementById("hasFrokostForSkole");
+const hasDrivingFemalePartner = document.getElementById(
+  "hasDrivingFemalePartner"
+);
+const hasMaleDrivingPartner = document.getElementById("hasMaleDrivingPartner");
+const isSpillerItimen = document.getElementById("isSpillerItimen");
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  calculateExactTimeOfDeath(
+    fodselsdato.value,
+    kjønn.value,
+    wowrace.value,
+    antallGangerRettetGeirSinProg.value,
+    treningPerUke.value,
+    McTjukkasPerManed.value,
+    hasFrokostForSkole.value,
+    hasDrivingFemalePartner.value,
+    hasMaleDrivingPartner.value,
+    isSpillerItimen.value,
+    form.value
+  );
+});
 
 /**
  * @param {String} fodselsdato format: ddmmyy
@@ -53,6 +75,7 @@ function calculateAge(
 
   return calculatedAge;
 }
+
 //Complete the formulae
 //You are supposed to calculate time and date of death, down to the minute!
 function calculateExactTimeOfDeath(
@@ -79,5 +102,19 @@ function calculateExactTimeOfDeath(
     isSpillerItimen
   );
 
-  return null; //fullfør funkjsonen
+  console.log(achievedAge);
+
+  // let today = new Date();
+  // let age = today - fodselsdato;
+
+  // let diffMs = achievedAge - age; // milliseconds
+  // let diffDays = Math.floor(diffMs / 86400000); // days
+  // let diffHrs = Math.floor((diffMs % 86400000) / 3600000); // hours
+  // let diffMins = Math.round(((diffMs % 86400000) % 3600000) / 60000); // minutes
+  // console.log(
+  //   diffDays + " days, " + diffHrs + " hours, " + diffMins + " minutes "
+  // );
+
+  return; //fullfør funkjsonen
 }
+
